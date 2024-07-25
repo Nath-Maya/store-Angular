a73517af0d1e8e5266f65583cb3a6d0a6ea39b64
Nath-Maya <117542812+Nath-Maya@users.noreply.github.com>
2024-07-25 11:36:15 -0500

    Render product in the component List

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from '../../../produts/components/product/product.component.js';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
